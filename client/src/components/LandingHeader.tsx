727dabb7391f24072631463f5193d57e22c6ebae
AbdelrahmanAhmed <404.abdelrahman@gmail.com>
2025-02-16 21:38:00 +0100

    edit landing nav

--- client/src/components/LandingHeader.tsx
+++ client/src/components/LandingHeader.tsx
@@ -27,14 +27,14 @@ function LandingHeader() {
                         </Link>
                     </div>
                     <div className="sm:hidden flex justify-center items-center" onClick={() => setNav(!nav)}>
-                        <div className="bg-white text-blue-800 px-3 py-2">
+                        <div className="bg-white text-blue-800 px-3 py-2 cursor-pointer">
                             <TfiMenu size={20}/>
                         </div>
                     </div>
                 </div>
                 <div className={nav ? "fixed top-0 right-0 w-[50%] h-[100vh] bg-white z-51 duration-150" : "fixed top-0 right-0 w-[50%] h-[100vh]  bg-white z-50 translate-x-full duration-150"}>
                    <div className="mx-4 my-5 w- h-full flex flex-col justify-start items-start gap-2">
-                       <IoMdClose size={25} className="self-end mx-2" onClick={() => setNav(!nav)}/>
+                       <IoMdClose size={25} className="self-end mx-2 cursor-pointer" onClick={() => setNav(!nav)}/>
                        <Link to="/signup">
                            <div className="bg-white text-blue-800 text-xl">
                                Sign Up
